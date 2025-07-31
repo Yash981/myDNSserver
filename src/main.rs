11ad32693f19efd6cbb350622784f594e6fc2aa6
Yashwanth <dyashwanth14011999@gmail.com>
2025-07-31 15:04:53 +0530

    write header section task

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,13 @@ fn main() {
     // Uncomment this block to pass the first stage
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
-    //
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                let response = b"msg from server hello";
                 udp_socket
-                    .send_to(&response, source)
+                    .send_to(response, source)
                     .expect("Failed to send response");
             }
             Err(e) => {
